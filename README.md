e9180649cb3e18d1bd7a99b62b942fb6b1d917e0
Aaron Parecki <aaron@parecki.com>
2012-03-29 13:57:05 -0700

    First commit of script and initial readme

---
+++ README.md
@@ -0,0 +1,16 @@
+Location Simulator
+==================
+
+This script is a utility for debugging and testing Geoloqi applications. You can replay a Geoloqi history file
+back into the API so that you can test the streaming API or geofencing with easily re-creatable data.
+
+Usage
+-----
+
+First, you need a source file. You can get a source file by using the location/history API method.
+See https://developers.geoloqi.com/api/location/history for documentation.
+
+You can use the History tab on the Geoloqi website to preview your results before downloading. Visit
+the website at https://geoloqi.com/map to see your history and find some data to download.
+
+
---
+++ location-update.rb
@@ -0,0 +1,113 @@
+require 'json'
+require 'geoloqi'
+require 'highline/import'
+require 'optparse'
+
+# Put in your client ID and secret from
+# https://developers.geoloqi.com
+LQ_CLIENT_ID = ''
+LQ_CLIENT_SECRET = ''
+
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: location-update.rb --file=history.json --rate=10 --start=0 --verbose"
+
+  opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+    options[:verbose] = v
+  end
+
+  opts.on("-t", "--token=[TOKEN]", "Access token") do |t|
+    options[:access_token] = t
+  end
+
+  opts.on("-s", "--start=[START]", Integer, "Start index") do |s|
+    options[:start] = s
+  end
+
+  opts.on("-r", "--rate=[RATE]", Integer, "Playback rate") do |r|
+    options[:playback_rate] = r
+  end
+
+  opts.on("-f", "--file=FILE", "History file") do |f|
+    options[:file] = f
+  end
+end.parse!
+
+if options[:file].nil?
+  puts "Usage: location-update.rb --file=filename.json"
+  exit!
+end
+
+if !FileTest.exist?(options[:file])
+  puts "File not found: #{options[:file]}"
+  exit!
+end
+
+playback_rate = options[:playback_rate] || 5
+
+if options[:access_token].nil?
+  username = ask("Enter your username:  ") { |q| q.echo = true }
+  password = ask("Enter your password:  ") { |q| q.echo = "*" }
+  geoloqi = Geoloqi::Session.new :access_token => nil, :config => {
+    :client_id => LQ_CLIENT_ID,
+    :client_secret => LQ_CLIENT_SECRET
+  }
+  geoloqi.establish :grant_type => 'password', :username => username, :password => password
+else
+  geoloqi = Geoloqi::Session.new :access_token => options[:access_token], :config => {
+    :client_id => LQ_CLIENT_ID,
+    :client_secret => LQ_CLIENT_SECRET
+  }
+end
+
+
+
+profile = geoloqi.get 'account/profile'
+puts 
+puts "Logged in as #{profile[:username]}"
+puts "Access token: #{geoloqi.access_token}"
+puts
+
+history = JSON.parse File.read(File.join(options[:file]))
+
+puts history['points'].length
+
+start_index = options[:start]
+
+while true do
+  last_point = nil
+  history['points'].each_with_index do |point,i|
+    if last_point.nil? || (start_index && i < start_index)
+      last_point = point
+      next
+    end
+
+    last_time = Time.at last_point['date_ts']
+    this_time = Time.at point['date_ts']
+
+    diff = this_time - last_time
+
+    diff = 300 if diff > 300
+
+    puts "Waiting #{diff} seconds..."
+
+    sleep diff / playback_rate
+
+    puts "Current point [#{i}] #{point['date_ts']} #{point['location']['position']['latitude']} #{point['location']['position']['longitude']}"
+
+    last_point = point.clone
+    new_point = point.clone
+
+    new_point.delete 'uuid'
+    new_point['date'] = Time.now
+    new_point.delete 'date_ts'
+
+    puts new_point.to_json if options[:verbose]
+
+    response = geoloqi.post 'location/update', [new_point]
+    puts response if options[:verbose]
+
+  end
+  start_index = 0
+end
+
